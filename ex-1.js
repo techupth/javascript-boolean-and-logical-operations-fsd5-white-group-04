ac5f32d99cfa22ded81dc683fd22aa338020fc33
Yupaporn R. <lyupaporn.r@gmail.com>
2023-11-24 12:22:39 +0700

    feat(boolean-and-logical): add completed exercise

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
 
 let isAllow;
 
 // Start coding here
 
+console.log(isAllow);
+
+isOver18 = true;
+hasCriminalBlacklist = false;
+isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
